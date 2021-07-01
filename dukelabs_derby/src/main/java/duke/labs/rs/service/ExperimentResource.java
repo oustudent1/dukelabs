774a9246dd91c248ab1d9c80cc78f91cdc20c339
oustudent1 <joe.greenwald@oracle.com>
2021-07-01 15:03:29 -0700

    302 update

--- dukelabs_derby/src/main/java/duke/labs/rs/service/ExperimentResource.java
+++ dukelabs_derby/src/main/java/duke/labs/rs/service/ExperimentResource.java
@@ -112,7 +112,7 @@ public class ExperimentResource {
 //        return rb.build();
 //    }
 private JsonObject createResponse() {
-    String msg =  "Service is Working and Responding**2:16";
+    String msg =  "Service is Working and Responding**3:02";
 
     return JSON.createObjectBuilder()
             .add("message", msg)
